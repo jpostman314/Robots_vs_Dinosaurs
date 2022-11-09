5f2d96ce637085c4097ecad5f57e4977253e1edc
Josh <jpostman314@yahoo.com>
2022-11-08 20:18:08 -0500

    Started writing logic for the bonus points user stories.

--- robot.py
+++ robot.py
@@ -6,9 +6,17 @@ class Robot:
         self.name = name
         self.active_weapon = active_weapon
         self.health = int(health)
-    
+       
+
+
+
     def attack(self, dinosaur):
-        self.active_weapon = Weapon("Laser", 10)
+        
+        laser = Weapon("Laser", 25)
+        flame_thrower = Weapon("Flame Thrower", 20)
+        toxic_gas = Weapon("Toxic Gas", 10)
+        active_weapon = input("Which weapon would you like the robot to attack with? [Toxic Gas, Flame Thrower, or Laser] ")
+        self.active_weapon = active_weapon
         dinosaur.health = dinosaur.health - int(self.active_weapon.attack_power)
         print("")
         print(f"{dinosaur.name}'s remaining health is {dinosaur.health}")
@@ -17,4 +25,5 @@ class Robot:
             print("")
             print(f"{dinosaur.name} has died!")
             print("")
-        
+    
+
