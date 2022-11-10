eb59f67d450c2dc8ea24bef28af7185710b5a898
Josh <jpostman314@yahoo.com>
2022-11-10 13:47:18 -0500

    Created the fleet class and herd class as a part of the bonus user stories

---
+++ fleet.py
@@ -0,0 +1,5 @@
+# This is to be the class for the robot fleet that will be a list of 3 robots to battle against the dinosaurs
+class Herd:
+
+    def __init__(self):
+        self.robots_list = []
---
+++ herd.py
@@ -0,0 +1,6 @@
+#This will be the class for the herd of dinosaurs that will be battling against the robot fleet!
+
+class Herd:
+
+    def __init__(self):
+        self.dinosaurs_list = []
